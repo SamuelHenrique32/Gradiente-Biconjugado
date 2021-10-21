c679ea7863736377f7a59fba9bd2445ff8315160
Samuel Henrique Dalmas <samuel.dalmas@mobitec.com.br>
2021-10-20 23:27:56 -0300

    feat: Using Library To Read Matrix Almost Finished

--- gradiente_biconjugado.c
+++ gradiente_biconjugado.c
@@ -9,8 +9,7 @@
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
-typedef struct
-{
+typedef struct {
   int i_size_ptr;
   int i_size_indexes;
   int i_size_values;
@@ -22,11 +21,11 @@ typedef struct
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
 void read_matrix(char *pc_file, int *pi_m, int *pi_n, int *pi_non_zeros, int **ppi_colptr, int **ppi_rows, double **ppd_values);
-void init_mat(double *pd_mat_a);
+matrix_hb_t* prepare_matrix(matrix_hb_t *ps_matrix);
 void init_vector(double *pd_vector, int i_n);
 double *aloc_vector(int i_size);
-void mult_mat_vector(int i_n, double *pd_mat_a, double *pd_vector, double *pd_result);
-void mult_mat_row_vector(int i_n, double *pd_mat_a, double *pd_vector, double *pd_result);
+void mult_mat_vector(int i_n, double *pd_val, int *pi_col, int *pi_ptr, double *pd_vet, double *pd_res);
+void mult_mat_row_vector(int i_n, double *pd_val, int *pd_row, int *pd_ptr, double *pd_vet, double *pd_res);
 double mult_vector_row_vector(int i_n, double *pd_vector1, double *pd_vector2);
 void sub_vector_vector(int i_n, double *pd_vector1, double *pd_vector2, double *pd_result);
 void copy_vector(int i_n, double *pd_vector1, double *pd_result);
@@ -108,39 +107,6 @@ matrix_hb_t* prepare_matrix(matrix_hb_t *ps_matrix) {
   return ps_matrix_b;
 }
 
-void init_mat(double *pd_mat_a) {
-
-  mat(0, 0) = 5;
-  mat(0, 1) = 0;
-  mat(0, 2) = 0;
-  mat(0, 3) = 2;
-  mat(0, 4) = 1;
-
-  mat(1, 0) = 0;
-  mat(1, 1) = 5;
-  mat(1, 2) = 0;
-  mat(1, 3) = 0;
-  mat(1, 4) = 2;
-
-  mat(2, 0) = 0;
-  mat(2, 1) = 0;
-  mat(2, 2) = 5;
-  mat(2, 3) = 0;
-  mat(2, 4) = 0;
-
-  mat(3, 0) = 2;
-  mat(3, 1) = 0;
-  mat(3, 2) = 0;
-  mat(3, 3) = 5;
-  mat(3, 4) = 0;
-
-  mat(4, 0) = 1;
-  mat(4, 1) = 2;
-  mat(4, 2) = 0;
-  mat(4, 3) = 0;
-  mat(4, 4) = 5;
-}
-
 void init_vector(double *pd_vector, int i_n) {
 
     for(int i_index=0; i_index<i_n; i_index++)  {
@@ -152,28 +118,31 @@ double *aloc_vector(int i_size) {
   return calloc(i_size, sizeof(double));
 }
 
-void mult_mat_vector(int i_n, double *pd_mat_a, double *pd_vector, double *pd_result) {
+void mult_mat_vector(int i_n, double *pd_val, int *pi_col, int *pi_ptr, double *pd_vet, double *pd_res) {
 
-    for(int i_index=0 ; i_index<i_n ; i_index++) {
+  int i_index = 0, i_j = 0;
 
-        pd_result[i_index] = 0;
+  for(i_index=0; i_index<i_n ; i_index++) {
 
-        for(int i_index2=0 ; i_index2<i_n ; i_index2++) {
-            pd_result[i_index] += mat(i_index, i_index2) * pd_vector[i_index2];
-        }
+    pd_res[i_index] = 0;
+    for(i_j=pi_ptr[i_index] - 1 ; i_j < pi_ptr[i_index + 1] - 1 ; i_j++) {
+        pd_res[i_index] += pd_val[i_j] * pd_vet[pi_col[i_j] - 1];
     }
+  }  
 }
 
-void mult_mat_row_vector(int i_n, double *pd_mat_a, double *pd_vector, double *pd_result) {
+void mult_mat_row_vector(int i_n, double *pd_val, int *pi_row, int *pi_ptr, double *pd_vet, double *pd_res)
+{
+  int i_index = 0, i_j = 0;
+  
+  for(i_index=0 ; i_index<i_n ; i_index++) {
 
-  for(int i_index=0 ; i_index<i_n ; i_index++) {
-
-    pd_result[i_index] = 0;
-
-    for(int i_index2=0; i_index2<i_n; i_index2++) {
-      pd_result[i_index] += mat(i_index2, i_index) * pd_vector[i_index2];
-    }
-  }
+    pd_res[i_index] = 0;
+    
+    for(i_j=(pi_ptr[i_index]-1) ; i_j<(pi_ptr[i_index+1] - 1) ; i_j++) {
+        pd_res[i_index] += pd_val[i_j] * pd_vet[pi_row[i_j] - 1];
+      }
+    }  
 }
 
 double mult_vector_row_vector(int i_n, double *pd_vector1, double *pd_vector2) {
@@ -231,26 +200,13 @@ int main(int argc, char **argv) {
 
   int i_imax = 1000000;
   int i_iteration = 1;
-  int i_n = kN_COLUMNS;
+  //int i_n = kN_COLUMNS;
   int i_M = 0, i_N = 0, i_non_zeros = 0;
-
-  double *pd_mat_a = aloc_vector(i_n * i_n); //Vector as a matrix
-  init_mat(pd_mat_a);
-
-  double *pd_vector_b = aloc_vector(i_n);
-  init_vector(pd_vector_b, i_n);
 
   //x = zeros(n,1);
   //p = zeros(n,1);
   //p2 = zeros(n,1)
-  double *pd_vector_x = aloc_vector(i_n);
-  double *pd_vector_p = aloc_vector(i_n);
-  double *pd_vector_p2 = aloc_vector(i_n);
 
-  double *pd_vector_r = aloc_vector(i_n);
-  double *pd_vector_aux = aloc_vector(i_n);
-  double *pd_vector_v = aloc_vector(i_n);
-  double *pd_vector_r2 = aloc_vector(i_n);
   double d_rho0;
   double d_beta;
   double d_alpha;
@@ -273,12 +229,24 @@ int main(int argc, char **argv) {
 
   ps_mat_csr = prepare_matrix(ps_mat_csc);
 
+  double *pd_vector_x = aloc_vector(i_N);
+  double *pd_vector_p = aloc_vector(i_N);
+  double *pd_vector_p2 = aloc_vector(i_N);
+
+  double *pd_vector_r = aloc_vector(i_N);
+  double *pd_vector_aux = aloc_vector(i_N);
+  double *pd_vector_v = aloc_vector(i_N);
+  double *pd_vector_r2 = aloc_vector(i_N);
+
+  double *pd_vector_b = aloc_vector(i_N);
+  init_vector(pd_vector_b, i_N);
+
   //r = b - A*x;
-  mult_mat_vector(i_n, pd_mat_a, pd_vector_x, pd_vector_aux);
-  sub_vector_vector(i_n, pd_vector_b, pd_vector_aux, pd_vector_r);
+  mult_mat_vector(i_N, ps_mat_csr->pd_values, ps_mat_csr->pi_indexes, ps_mat_csr->pi_pointers, pd_vector_x, pd_vector_aux);
+  sub_vector_vector(i_N, pd_vector_b, pd_vector_aux, pd_vector_r);
 
   //r2 = r;
-  copy_vector(i_n, pd_vector_r, pd_vector_r2);
+  copy_vector(i_N, pd_vector_r, pd_vector_r2);
 
   //rho = 1;
   double d_rho = 1;
@@ -289,29 +257,29 @@ int main(int argc, char **argv) {
     //rho = r2' * r;
     //beta = rho / rho0
     d_rho0 = d_rho;
-    d_rho = mult_vector_row_vector(i_n, pd_vector_r2, pd_vector_r);
+    d_rho = mult_vector_row_vector(i_N, pd_vector_r2, pd_vector_r);
     d_beta = d_rho / d_rho0;
 
     //p = r + beta*p;
     //p2 = r2 + beta*p2;
-    for(int i_index=0; i_index<i_n; i_index++) {
+    for(int i_index=0; i_index<i_N; i_index++) {
 
       pd_vector_p[i_index] = pd_vector_r[i_index] + d_beta * pd_vector_p[i_index];
       pd_vector_p2[i_index] = pd_vector_r2[i_index] + d_beta * pd_vector_p2[i_index];
     }
 
-    //v = A * p;
-    mult_mat_vector(i_n, pd_mat_a, pd_vector_p, pd_vector_v);
+    //v = A * p;    
+    mult_mat_vector(i_N, ps_mat_csr->pd_values, ps_mat_csr->pi_indexes, ps_mat_csr->pi_pointers, pd_vector_p, pd_vector_v);
 
     //alpha = rho/(p2'*v)
-    d_alpha = d_rho / mult_vector_row_vector(i_n, pd_vector_p2, pd_vector_v);
+    d_alpha = d_rho / mult_vector_row_vector(i_N, pd_vector_p2, pd_vector_v);
 
     //x = x + alpha*p;
-    for(int i_index=0 ; i_index<i_n ; i_index++) {
+    for(int i_index=0 ; i_index<i_N ; i_index++) {
       pd_vector_x[i_index] = pd_vector_x[i_index] + d_alpha * pd_vector_p[i_index];
     }
 
-    d_calculated_error = mult_vector_row_vector(i_n, pd_vector_r, pd_vector_r);
+    d_calculated_error = mult_vector_row_vector(i_N, pd_vector_r, pd_vector_r);
 
     if(d_calculated_error < (d_error*d_error)) {
       break;
@@ -319,8 +287,8 @@ int main(int argc, char **argv) {
 
     //r = r - alpha * v;
     //r2 = r2 - alpha *A' * p2;
-    mult_mat_row_vector(i_n, pd_mat_a, pd_vector_p2, pd_vector_aux);
-    for(int i_index=0 ; i_index<i_n ; i_index++) {
+    mult_mat_row_vector(i_N, ps_mat_csc->pd_values, ps_mat_csc->pi_indexes, ps_mat_csc->pi_pointers, pd_vector_p2, pd_vector_aux);
+    for(int i_index=0 ; i_index<i_N ; i_index++) {
 
       pd_vector_r[i_index] = pd_vector_r[i_index] - d_alpha * pd_vector_v[i_index];
       pd_vector_r2[i_index] = pd_vector_r2[i_index] - d_alpha * pd_vector_aux[i_index];
@@ -334,7 +302,24 @@ int main(int argc, char **argv) {
 
   printf("Iteracoes = %d\n\n", i_iteration);
   printf("Resposta:\n");
-  print_vector(i_n, pd_vector_x);
+  print_vector(i_N, pd_vector_x);
+
+  free(ps_mat_csr->pd_values);
+  free(ps_mat_csr->pi_pointers);
+  free(ps_mat_csr->pi_indexes);
+
+  free(ps_mat_csc->pd_values);
+  free(ps_mat_csc->pi_pointers);
+  free(ps_mat_csc->pi_indexes);
+
+  free(pd_vector_b);
+  free(pd_vector_x);
+  free(pd_vector_p);
+  free(pd_vector_p2);
+  free(pd_vector_r);
+  free(pd_vector_aux);
+  free(pd_vector_v);
+  free(pd_vector_r2);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
